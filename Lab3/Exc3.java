cf2e5a441be6b3de855a90e4523eddf5174d6d8a
STUDENT Kass Smith <Kass.Smith@students.ittralee.ie>
2023-09-19 12:46:23 +0100

    Signed-off-by: STUDENT Kass Smith <Kass.Smith@students.ittralee.ie>

---
+++ Lab3/Exc3.java
@@ -0,0 +1,53 @@
+package Lab3;
+
+import java.util.Scanner;
+
+public class Exc3 {
+
+    public static void main(String[] args){
+
+        Scanner input = new Scanner(System.in);
+
+        int charCount = 0, letterCount = 0, digitCount = 0, wordCount = 0, puncCount = 0, sentCount = 0;
+
+        System.out.println("Please enter your line of text: \n\n");
+        String sent = input.nextLine();
+
+        for (int x = 0; x < sent.length(); x++){
+            char current = sent.charAt(x);
+
+            charCount ++;
+
+            if (Character.isAlphabetic(current)){
+                letterCount ++;
+            }
+            else if (Character.isDigit(current)){
+                digitCount ++;
+            }
+
+            if (current == ' '){
+                wordCount ++;
+            }
+
+            if (".!'".indexOf(current) != -1){
+                puncCount ++;
+                sentCount ++;
+            }
+        }
+
+        wordCount ++;
+
+        System.out.println("\n\n============================================\n" +
+                "\t\tLexical Analysis of Your Text\n" +
+                "============================================\n\n");
+
+        System.out.println(String.format("Characters: %d\n" +
+                "Letters: %d\n" +
+                "Digits: %d\n" +
+                "Words: %d\n" +
+                "Sentences: %d\n" +
+                "Punctuation symbols: %d",
+                charCount, letterCount, digitCount, wordCount, sentCount, puncCount));
+
+    }
+}
